45f1a44e8e4955024b12def4f7debfcf7a10f91f
luisfelipe118 <74427648+luisfelipe118@users.noreply.github.com>
2021-09-27 16:02:31 -0500

    Update form.js

--- form.js
+++ form.js
@@ -9,39 +9,6 @@ function checkNombre(valor){
 }
 module.exports = checkNombre;
 
-function checkCorreo(valor){
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(valor))
-    {
-        return (true);
-    }
-    
-    return (false);
-}
-
-module.exports = checkCorreo;
-
-// Validación  de la contraseña, en donde 
-// el usuario deba  de ingresar por lo 
-// menos  una letra mayúscula,  una 
-// minúscula,  un número  y con una 
-// longitud  mayor  o igual a 8 dígitos. Este 
-// campo  es requerido,  por lo que, no se 
-// puede  dejar vacío.
-
-function checkContrasena(password) {
-    const expresion = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,30}$/g
-    const pattern = password.match(expresion)
-    if (pattern === null) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-
-module.exports = checkContrasena;
-
-
 // Validar  el campo  teléfono, este 
 // campo  debe tener una longitud  de 
 // 7 dígitos únicamente,  solo puede 
@@ -79,4 +46,39 @@ function checkDir(direc){
 }
 
 module.exports = checkDir;
+
+function checkCorreo(valor){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(valor))
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+module.exports = checkCorreo;
+
+// Validación  de la contraseña, en donde 
+// el usuario deba  de ingresar por lo 
+// menos  una letra mayúscula,  una 
+// minúscula,  un número  y con una 
+// longitud  mayor  o igual a 8 dígitos. Este 
+// campo  es requerido,  por lo que, no se 
+// puede  dejar vacío.
+
+function checkContrasena(password) {
+    const expresion = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,30}$/g;
+    const pattern = password.match(expresion);
+    if (pattern === null) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+module.exports = checkContrasena;
+
+
+
 

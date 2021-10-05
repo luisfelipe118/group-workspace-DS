1a94d6d072aa7e0b6ef247fe7bbb0bf89e87fb6f
JuliaCam21 <juliancamilogarciaescobar7@gmail.com>
2021-10-05 17:36:57 -0500

    update registro.js
    hola , te propongo esta version de registro.js , si deseas tomarla en cuenta , ya lo probe y funciona igual la diferencia es que a mi parecer es un poco mas legible y corto
    si no quieres actualizar no hay problema

--- registro.js
+++ registro.js
@@ -1,77 +1,41 @@
 let registros = [];
 
-function ordenarArreglo(arreglo){
-    let newArray = [];
-    if (arreglo.length > 0) {
-        newArray = arreglo.sort(function(a, b){
-            let x = a.Nombre.toLowerCase(), 
-                y = b.Nombre.toLowerCase();
-            if (x < y) {return -1;}
-            if (x > y) {return 1;}
-            return 0;
-        });
+function agregarRegistro() {
 
-        for( let item in arreglo){
-            console.log(arreglo[item].Nombre);
-        }
-    }
-    return newArray;
-}
-
-function filtrarCorreo(arreglo){
-    arreglo = registros;
-    let newArray = arreglo.filter(function (el) {
-        return el.Correo.includes("gmail.com") ;
-    });
-
-    for( let item in newArray){
-        console.log(newArray[item]);
-    }
-
-    return newArray;
-}
-
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
-}
-
-function agregarRegistro(){
-    /*let Nombre = document.getElementById('Nombre').value;  
-    let Genero = document.querySelector('input[name = "Genero"]:checked').value;
-    let Telefono = document.getElementById('Telefono').value;
-    let Direccion = document.getElementById('Direccion').value;  
-    let Correo = document.getElementById('Correo').value;
-    let Contrasena = document.getElementById('Contrasena').value;*/
-
-    let Nombre = "Ana";  
-    let Genero = "";
-    let Telefono = "";
-    let Direccion = "";
-    let Correo = "asgmail.com";
-    let Contrasena = "";
-
-    let registro = {
-        Nombre,
-        Genero,
-        Telefono,
-        Direccion,
-        Correo,
-        Contrasena
-    }
-
-    console.log(registro);
+  let registro = {
+    Nombre: "Ana",
+    Genero: "",
+    Telefono: "",
+    Direccion: "",
+    Correo : "Ana@gmail.com",
+    Contrasena: "",
+  };
+  
     if (registros.length < 3) {
-        registros.push(registro);
-    } 
+      registros.push(registro);
+    }   
+  return registros;
 }
 
-//cj.cardona@hotmail.com
+function ordenarArreglo(arreglo) {
+  let names = arreglo.map((x)=> {
+    return x.Nombre
+  }).sort()
+  let response = []
+  for (let i=0; i<names.length; i++) {
+    let found = arreglo.find(element => element.Nombre == names[i])
+    response.push(found)
+  }
+  return response;
+}
 
-module.exports = { registros, agregarRegistro, ordenarArreglo, filtrarCorreo };
+function filtrarCorreo(arreglo) {
+  let response = arreglo.filter(ele => ele.Correo.indexOf('@gmail') !== -1);
+  return response
+}
+module.exports = {
+  registros,
+  agregarRegistro,
+  filtrarCorreo,
+  ordenarArreglo,
+}

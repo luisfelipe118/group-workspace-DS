0465dad4d0345ccdadcb83c0dd4b4825b4f731fe
yar0th3ungg0y <51716553+yar0th3ungg0y@users.noreply.github.com>
2021-10-13 14:36:38 -0500

    Commit login.js primera version

---
+++ login.js
@@ -0,0 +1,18 @@
+let registros = [];
+
+function Login(){
+
+}
+
+function agregarRegistro(){
+
+}
+
+function validarCAPTCHA(){
+
+}
+
+module.exports.login = login;
+module.exports.registros = registros;
+module.exports.validarCAPTCHA = validarCAPTCHA;
+module.exports.agregarRegistro = agregarRegistro;
